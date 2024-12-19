f41cabc4790412f742855c5cb0724462414529d3
ChaGyoungtae <113877917+ChaGyoungtae@users.noreply.github.com>
2024-12-19 11:18:39 +0900

    [Bronze V] Title: 합, Time: 100 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -1,17 +1,21 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
 
+
+        int a = Integer.parseInt(br.readLine());
         int sum = 0;
-
-        for(int i = 1; i<= t; i++){
+        for (int i = 1 ; i <= a ; i ++){
             sum += i;
         }
         System.out.println(sum);
+
+
     }
 
 }
