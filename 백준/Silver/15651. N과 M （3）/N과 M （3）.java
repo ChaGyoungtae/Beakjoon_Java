8ba5f5a367c93b71eef8e0f7e503c1bd9ef39349
ChaGyoungtae <113877917+ChaGyoungtae@users.noreply.github.com>
2025-01-22 09:08:31 +0900

    [Silver III] Title: N과 M (3), Time: 408 ms, Memory: 68164 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+    static int n;
+    static int m;
+    static StringBuilder sb;
+    static int[] arr;
+
+    public static void btc(int at, int depth){
+        if(depth == m){
+            for(int e: arr){
+                sb.append(e).append(' ');
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 1; i <= n; i++){
+            arr[depth] = i;
+            btc(i+1, depth+1);
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        // 1 부터 N 까지 중복없이 count개 를 고른 수열
+        arr = new int[m];
+        btc(1, 0);
+
+
+        System.out.println(sb.toString());
+
+    }
+}
